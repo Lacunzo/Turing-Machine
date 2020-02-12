5759d91410a7726cb5eec0bd6b9f607297fca4f8
Lacunzo <50136299+Lacunzo@users.noreply.github.com>
2020-02-13 00:20:16 +0100

    tests prog

--- Tp1.java
+++ Tp1.java
@@ -13,7 +13,18 @@ public class Tp1 {
 		 prog.add(3,3,true,"r");
 		 prog.add(3,0,false,"le");
 		 tm.process(prog);
+		 System.out.print("On effectue l'addition");
 		 System.out.print(tm);
-		 /*exercices supplémentaire suppr et paire*/
+		 Programm prog2 = new Programm("suppr",4);
+		 prog2.add(1,1,false,"r");
+		 prog2.add(1,2,true,"er");
+		 prog2.add(2,2,true,"er");
+		 prog2.add(2,0,false,"");
+		 Programm prog3 = new Programm("paire",4);
+		 prog3.add(1,1,false,"r");
+		 prog3.add(1,2,true,"r");
+		 prog3.add(2,1,true,"r");
+		 prog3.add(2,0,false,"w");
+		 
 	}
 }
