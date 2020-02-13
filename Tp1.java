c072065132839fa1cdca1f870fd834f94d061ec0
Lacunzo <50136299+Lacunzo@users.noreply.github.com>
2020-02-13 10:02:54 +0100

    Addition  suppression  et  paire

--- Tp1.java
+++ Tp1.java
@@ -13,18 +13,36 @@ public class Tp1 {
 		 prog.add(3,3,true,"r");
 		 prog.add(3,0,false,"le");
 		 tm.process(prog);
-		 System.out.print("On effectue l'addition");
-		 System.out.print(tm);
+		 System.out.println("On effectue l'addition");
+		 System.out.println(tm);
+		 
+		 
+		 Turing tm2 = new Turing(14);
+		 tm2.writeTape("00111011111000");
+		 System.out.println("\n On utilise ce ruban");
+		 System.out.println(tm2);
 		 Programm prog2 = new Programm("suppr",4);
 		 prog2.add(1,1,false,"r");
 		 prog2.add(1,2,true,"er");
 		 prog2.add(2,2,true,"er");
 		 prog2.add(2,0,false,"");
-		 Programm prog3 = new Programm("paire",4);
-		 prog3.add(1,1,false,"r");
-		 prog3.add(1,2,true,"r");
-		 prog3.add(2,1,true,"r");
-		 prog3.add(2,0,false,"w");
+		 tm2.process(prog2);
+		 System.out.println("On effectue la suppression");
+		 System.out.println(tm2);
 		 
+		 Turing tm3 = new Turing(14);
+		 tm3.writeTape("00111110000000");
+		 System.out.println("\n On utilise ce ruban");
+		 System.out.println(tm3);
+		 Programm prog3 = new Programm("paire",6);
+		 prog3.add(1,1,false,"r");
+		 prog3.add(1,3,true,"r");
+		 prog3.add(2,3,true,"r");
+		 prog3.add(2,0,false,"");
+		 prog3.add(3,2,true,"r");
+		 prog3.add(3,0,false,"w");
+		 tm3.process(prog3);
+		 System.out.println("On effectue la paire");
+		 System.out.println(tm3);
 	}
 }
